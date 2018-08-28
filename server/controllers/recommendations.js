8582c7cf68c844a5884941eb00299e5b1d6bcb90
ligcasan <ligcasan@gmail.com>
2018-08-28 21:02:24 +0800

    Initial draft for generating recommendations

--- server/controllers/recommendations.js
+++ server/controllers/recommendations.js
@@ -1,44 +1,57 @@
+const decision_row = require('../models').user_decision;
 const g = require('ger');
+
+var fetch_todays_user_decision = function(status = 'like') {
+  const today = new Date().setHours(0,0,0,0);
+
+  return decision_row.findAll({
+    where: {
+      createdAt: {
+        gt: today
+      },
+      status: 'like' 
+    }
+  });
+}
+
+var parse_user_decision_for_ger = function(data, namespace = 'images') {
+  var obj = [];
+  data.forEach((row) => {
+    obj.push({
+      namespace: namespace,
+      person: row['dataValues']['user_id'],
+      action: row['dataValues']['status'],
+      thing: row['dataValues']['image_id'],
+      expires_at: '2020-06-06'
+    });
+  });
+  return obj;
+}
 
 module.exports = {
   generate(req, res) {
     var esm = new g.MemESM();
     var ger = new g.GER(esm);
+    var namespace = 'images';
 
-    ger.initialize_namespace('movies')
-    .then( function() {
-      return ger.events([
-        {
-          namespace: 'movies',
-          person: 'bob',
-          action: 'likes',
-          thing: 'xmen',
-          expires_at: '2020-06-06'
-        },
-        {
-          namespace: 'movies',
-          person: 'bob',
-          action: 'likes',
-          thing: 'avengers',
-          expires_at: '2020-06-06'
-        },
-        {
-          namespace: 'movies',
-          person: 'alice',
-          action: 'likes',
-          thing: 'xmen',
-          expires_at: '2020-06-06'
-        },
-      ])
-    })
-    .then( function() {
-      // What things might alice like?
-      return ger.recommendations_for_person('movies', 'alice', {actions: {likes: 1}, "filter_previous_actions": ["likes"],})
-    }).then( function(recommendations) {
-      console.log("\nRecommendations For 'alice'")
-      console.log(JSON.stringify(recommendations,null,2))
-    });
-
+    fetch_todays_user_decision()
+    .then((data) => parse_user_decision_for_ger(data))
+    .then(
+      (data) => {
+        console.log('Bye');
+        console.log(data);
+        ger.initialize_namespace(namespace)
+        .then( function() {
+          return ger.events(data)
+        })
+        .then( function() {
+          return ger.recommendations_for_person(namespace, 'cwok8Wj84KZBfFwwTMqvAq82Gf53', {actions: {like: 1}, "filter_previous_actions": ["like"]})
+        }).then( function(recommendations) {
+          console.log("\nRecommendations For cwok8Wj84KZBfFwwTMqvAq82Gf53")
+          console.log(JSON.stringify(recommendations,null,2))
+        });
+      }
+    );
     return res.status(201).send({'message': 'Successful T.'});
   },
 }
