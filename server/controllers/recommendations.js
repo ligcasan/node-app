55363f08a527a87b23747bfaeb8b92765657acb1
ligcasan <ligcasan@gmail.com>
2018-09-03 09:53:08 +0800

    Add /api/recommendations/generate endpoint
    Save recommended image id's for each user id

--- server/controllers/recommendations.js
+++ server/controllers/recommendations.js
@@ -1,9 +1,11 @@
 const decision_row = require('../models').user_decision;
 const g = require('ger');
 const sequelize = require('sequelize');
+const recommendation_row = require('../models').recommendation;
 
 var fetch_todays_users = function() {
-  const today = new Date().setHours(0,0,0,0);
+  // const today = new Date().setHours(0,0,0,0);
+  const today = new Date('2018-08-30').setHours(0,0,0,0);
 
   return decision_row.findAll({
     attributes: [[sequelize.fn('DISTINCT', sequelize.col('user_id')), 'user_ids']],
@@ -16,7 +18,8 @@ var fetch_todays_users = function() {
 }
 
 var fetch_todays_user_decision = function(status = 'like') {
-  const today = new Date().setHours(0,0,0,0);
+  // const today = new Date().setHours(0,0,0,0);
+  const today = new Date('2018-08-30').setHours(0,0,0,0);
 
   return decision_row.findAll({
     where: {
@@ -71,18 +74,32 @@ module.exports = {
                 ger.recommendations_for_person(
                   namespace,
                   item,
-                  {actions: {like: 1}, "filter_previous_actions": ["like"]}));
+                  {actions:
+                    {like: 1},
+                    "filter_previous_actions": ["like"]}));
             });
             return user_recommendations;
           }).then( function(recommendations) {
-            // console.log("\nRecommendations For cwok8Wj84KZBfFwwTMqvAq82Gf53")
-            // console.log(JSON.stringify(recommendations,null,2))
-var cnt = 0;
-Promise.all(recommendations).then(function(values) {
-console.log(values);
-});
+            Promise.all(recommendations).then(function(values) {
+	      console.log(values);
+              values.forEach((item) => {
+	        if(item.recommendations.length != 0) {
+		  var userId = '';
+		  for(var neighbhour in item.neighbourhood) {
+		    if(item.neighbourhood[neighbhour] == 1) {
+		      userId = neighbhour; 
+		    }
+		  }
+                  item.recommendations.forEach((thing) => {
+		    recommendation_row.create({
+		      'user_id': userId,
+		      'image_id': thing.thing
+		    });
+		  });
+		}
+	      });
+            });
             console.log('Bye');
-            console.log(user_ids);
           });
         }
       );
